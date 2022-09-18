71a66d5567f262341535354e443b177b6718a828
Uladzislau_Komar <vladkomar25@gmail.com>
2022-09-18 22:47:57 +0300

    Added segues

---
+++ Right on target/SecondViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SecondViewController.swift
+//  Right on target
+//
+//  Created by Uladzislau Komar on 18.09.22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        print("Second loadView")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Second viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Second viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Second viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Second viewDidDisappear")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -10,51 +10,75 @@ import UIKit
 class ViewController: UIViewController {
     // Загаданное число, раунт, сумма очков
     var number: Int = 0
-    var round: Int = 0
+    var round: Int = 1
     var points: Int = 0
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+        
+        let versionLabel = UILabel(frame: CGRect(x: 20, y: 20, width: 200, height: 20))
+        versionLabel.text = "Version 1.1"
+        self.view.addSubview(versionLabel)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+
 
     @IBOutlet var label: UILabel!
     @IBOutlet var slider: UISlider!
     
     @IBAction func checkNumber() {
-        if self.round == 0 {
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
-            self.round += 1
+        let numSlider = Int(self.slider.value.rounded())
+        
+        if numSlider > self.number {
+            self.points += 50 - numSlider + self.number
+        } else if numSlider < self.number {
+            self.points += 50 - self.number + numSlider
         } else {
-            let numSlider = Int(self.slider.value.rounded())
-            
-            if numSlider > self.number {
-                self.points += 50 - numSlider + self.number
-            } else if numSlider < self.number {
-                self.points += 50 - self.number + numSlider
-            } else {
-                self.points += 50
-            }
-            
-            if self.round == 5 {
-                let alert = UIAlertController(title: "Раунт закончен",
-                                              message: "Ваш счет \(self.points)",
-                                              preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Начать заново?",
-                                              style: .default,
-                                              handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
-            } else {
-                self.round += 1
-            }
-            
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
+            self.points += 50
         }
+        
+        if self.round == 5 {
+            let alert = UIAlertController(title: "Раунт закончен",
+                                          message: "Ваш счет \(self.points)",
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Начать заново?",
+                                          style: .default,
+                                          handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.round = 1
+            self.points = 0
+        } else {
+            self.round += 1
+        }
+        
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
     }
-
 }
 
